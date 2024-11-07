ced3e0c6a0bf8c70c68805c83f5634fbbc51c85c
Venuteja19 <venupattapu19@gmail.com>
2024-11-07 21:07:20 +0530

    Add files via upload

---
+++ Example2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Example2
+{
+ public static void main(String args[])
+ {
+  Scanner s=new Scanner(System.in);
+  System.out.println("Enter a value :");
+  int a=s.nextInt();
+  if(a==2) 
+  System.out.print("Hello:");
+ }
+}
