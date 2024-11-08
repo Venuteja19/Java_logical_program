dfa92895955c8a6c5fc55a2394537c89f3e897ab
Venuteja19 <venupattapu19@gmail.com>
2024-11-08 19:35:15 +0530

    Add files via upload

---
+++ ESnum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class ESnum
+{
+public static void main(String args[])
+{
+ Scanner sc=new Scanner(System.in);
+ int i=1,n,;
+ System.out.println("Enter i value");
+ int n=sc.nextInt();
+ do
+{
+  if(i%2==0)
+  System.out.println(i);
+  i++;
+{
+  while(i<=n);
+}
+}
