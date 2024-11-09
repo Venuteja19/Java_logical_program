a98605f26e5fcf14c68d21f69ead048dd26046ed
Venuteja19 <venupattapu19@gmail.com>
2024-11-09 21:50:35 +0530

    Add files via upload

---
+++ Exprg.java
@@ -0,0 +1,14 @@
+import java.util.*
+class Exprg
+{
+ public static void main(String args[])
+ Scanner sc=new Scanner(system.in)
+ int i,j;
+ i=sc.newInt();
+ j=sc.newInt();
+ for(i=1;i<=5;i++)
+{
+ for(j=1;j<=5;j++)
+ System.out.println("*");
+}
+}
