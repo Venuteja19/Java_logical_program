918a9384bf87b13e5f0f8defeb1e5eddb4031ceb
Venuteja19 <venupattapu19@gmail.com>
2024-11-11 22:06:58 +0530

    Add files via upload

---
+++ Samplesum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Samplesum
+{
+ public static void main(String args[])
+ {
+  Scanner sc=new Scanner(System.in);
+  int i,j,p=0;
+  System.out.println("The Number from" + 1 "to" j +"is :" );
+  j=sc.nextInt();
+  for(i=1;i<=j;i++)
+  p=p+i;
+  System.out.println("The Sum of"+1+"to"j+"is"+p);
+ }
+}
