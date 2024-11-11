8cf0f40a0ca92db8e488e42ea054664212a51ff9
Venuteja19 <venupattapu19@gmail.com>
2024-11-11 19:45:16 +0530

    Add files via upload

---
+++ Searcharr.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class Searcharr
+{
+  public static void main(String args[])
+{
+  Scanner sc=new Scanner(System.in);
+  int i=0;
+  int a=new int[n];
+  System.out.println("Enter Elements in Array :");
+  for(i=0;i<a.length;i++)
+  a[i]=sc,nextInt();
+ {
+  System.out.println("Elements in Array is :");
+ for(i=0;i<a.length;i++)
+ System.out.println(a[i]);
+}
+ System.out.println("Enter value for search");
+ int ser=sc.nextInt();
+ for(i=0;i<n;i++)
+}
+ if(ser==a[i])
+{
+  System.out.println(ser+"is found");
+  break;
+}
+ if(i==n)
+  System.out.println(ser+"is not found");
+}
+ 
