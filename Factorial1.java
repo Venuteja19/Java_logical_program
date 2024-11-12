7c5c622504264aa8e6da31e64888d7b55c70a997
Venuteja19 <venupattapu19@gmail.com>
2024-11-12 17:35:34 +0530

    Add files via upload

---
+++ Factorial1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Factorial1
+{
+  int i,j,f;
+  int fact(int a)
+{
+    i=a;
+    i<=j;
+    f=f*i;
+    i++;
+    retutn f;
+  } 
+ }
+    class main
+{
+   public static void main(String[] args)
+{
+   int b=1;c;
+   Factorial1 f1=new Factorial1;
+   f1.fact(b);
+   System.out.println("The Factorial of numbers is :"+c);
+  }  
+ }
