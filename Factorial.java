349f4aa9e5645003d4730156927ef59b5c512784
Venuteja19 <venupattapu19@gmail.com>
2024-11-12 17:12:07 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,20 @@
+ import java.util.Scanner;
+ class Factorial
+{
+  int i,j,k;
+  void fact()
+ {
+  for(i=1;i<=j;i++)
+  k=1;
+  k=k*i;
+  System.out.println("The Factorial value is :"+k);
+ }
+}
+   class main
+  {
+   public static void main(String args[])
+{
+   Factorial f1=new Factorial();
+   f1.fact();
+ }
+}
