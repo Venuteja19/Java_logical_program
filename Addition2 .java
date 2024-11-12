92bd8b61cfb6d429bbeee639eee848d184c5bc16
Venuteja19 <venupattapu19@gmail.com>
2024-11-12 17:44:25 +0530

    Add files via upload

---
+++ Addition2 .java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Addition2
+{
+ int a,b,c;
+ int add()
+{
+  a=50;
+  b=100;
+  c=a+b;
+  return c;
+ }
+}
+  class main
+{
+  public static void main(String[] args)
+{
+  int t;
+  Addition2 a1=new Addition2();
+  t=a2.add();
+  system.out.println("Addition of numbers is :"+t);
+ }
+}
