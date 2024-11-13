161ea6d9ca067ef9ccae89c3b30cd23322f19b52
Venuteja19 <venupattapu19@gmail.com>
2024-11-13 17:35:56 +0530

    Add files via upload

---
+++ Exdemo.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Exdemo
+{
+ Static int i;n=1;f=1;
+ Static void demo()
+{
+  for(i=1;i<=n;i++)
+  f=f*i;  
+}
+  System.out.println("The Factorial value is :"+f);
+ }
+}
+  class Demo
+{
+   public static void main(String[] args)
+{
+   Exdemo.demo();
+  }
+ }
+}
