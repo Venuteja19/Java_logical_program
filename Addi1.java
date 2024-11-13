8fe102477f93e7b8d58dc3682e88d9e50132d2eb
Venuteja19 <venupattapu19@gmail.com>
2024-11-13 17:49:32 +0530

    Add files via upload

---
+++ Addi1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Addi1
+{
+   int a,b,c;
+   a=10;
+   b=20;
+   c=a+b;
+   System.out.println("The Addition value is :"+c);
+ }
+  class Mod
+{
+  public static void main(String[] args)
+{
+  Addi1 a1=new Addi1();
+ }
+}
