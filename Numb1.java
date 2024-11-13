79de1764922867630f8fa386d2446179bd06fbd3
Venuteja19 <venupattapu19@gmail.com>
2024-11-13 21:04:58 +0530

    Add files via upload

--- Numb1.java
+++ Numb1.java
@@ -1,10 +1,13 @@
-class Numb1
+ import java.util.Scanner;
+ class Numb1
 {
  public static void main(String args[])
  {
- int i=0;
- while(i++<=100)
- if(i%2==1)
+ int i;
+ Scanner sc=new Scanner(System,in)
+ int n=sc.nextInt();
+ if(i=1;i<=n;i++)
+ if(i%2!=0)
  {
    System.out.println(i);
   }
