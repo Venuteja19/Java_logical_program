75a8294c64e999cc504d0c6bc157f8d639e65c69
Venuteja19 <venupattapu19@gmail.com>
2024-11-13 17:40:45 +0530

    Add files via upload

---
+++ Block.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Block
+{
+ public static void main(String[] args)
+{
+   System.out.println("It's static method");
+   System.exit(0);
+   System.out.println("It's main method");
+  }
+ }
