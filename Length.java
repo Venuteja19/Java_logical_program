3933ca654fa2cccc4c19be11cc790053a13b9e9e
Venuteja19 <venupattapu19@gmail.com>
2024-11-13 19:32:53 +0530

    Add files via upload

---
+++ Length.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Length
+{
+ public static void main(String args[])
+{
+  int i,j=0;
+  char n[]={10};
+  System.out.println("Enter a String");
+  for(i=0;n[i]!='\0';i++)
+  j++;
+   System.out.println("The length of"+ n +"is"+j);
+ }
+}
