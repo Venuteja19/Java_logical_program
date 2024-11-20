dd0ce3d604c80463ec72718d8379c596d5c89d9a
Venuteja19 <venupattapu19@gmail.com>
2024-11-20 17:48:38 +0530

    Add files via upload

---
+++ Mcities.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+public class Mcities
+{
+ public static void main(String args[])
+{
+ LinkedList<String>cities=new LinkedList();
+ cities.add("Vijayawada");
+ cities.add("Vizag");
+ cities.add("Guntur");
+ cities.add("Gudivada");
+ System.out.println("Initial LinkedList :"+cities);
+ }
+}
